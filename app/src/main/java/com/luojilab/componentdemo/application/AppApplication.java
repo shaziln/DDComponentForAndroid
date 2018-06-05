a30dc46ad08b7b0dc131d787a9b11a54d0b2dd3d
Rona <luona01@chinatopcredit.com>
2018-06-05 17:11:26 +0800

    升级应用插件，依赖库后，可以运行app

--- app/src/main/java/com/luojilab/componentdemo/application/AppApplication.java
+++ app/src/main/java/com/luojilab/componentdemo/application/AppApplication.java
@@ -14,10 +14,6 @@ public class AppApplication extends BaseApplication {
         super.onCreate();
 
         UIRouter.getInstance().registerUI("app");
-
-        //如果isRegisterCompoAuto为false，则需要通过反射加载组件
-//        Router.registerComponent("com.luojilab.reader.applike.ReaderAppLike");
-//        Router.registerComponent("com.luojilab.share.applike.ShareApplike");
     }
 
 
